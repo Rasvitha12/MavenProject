f43691dc6a964bccc75cd565272e400e31d277a7
madhuganesh13 <rasivitha98@gmail.com>
2026-02-17 23:13:48 +0530

    FirstPush

---
+++ src/test/java/com/omrbranch/SampleGit.java
@@ -0,0 +1,17 @@
+package com.omrbranch;
+
+public class SampleGit {
+
+	public void classA() {
+		// TODO Auto-generated method stub
+
+	}
+	public void classB() {
+		// TODO Auto-generated method stub
+
+	}
+	public void classC() {
+		// TODO Auto-generated method stub
+
+	}
+}
